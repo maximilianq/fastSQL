1fa2c41d8982d68cb91240dc4ebdfb8fa999ae0a
Maximilian Quaeck <maximilian.quaeck@gmx.de>
2023-03-12 00:05:05 +0100

    initial commit.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="fastsql",
+    version='0.0.0.1',
+    description="Library for SQL integration using annotations.",
+    author="Maximilian Quaeck",
+    author_email="maximilian.quaeck@gmx.de",
+    keywords="sql",
+    packages=['fastsql'],
+    package_dir={'fastsql': 'src'},
+    install_requires=[
+        "psycopg-binary"
+    ]
+)
---
+++ src/fastsql.py
@@ -0,0 +1,40 @@
+from psycopg import connect, connection
+
+from typing import List, Dict, Optional, Union
+
+
+class FastSQL:
+
+    def __init__(self, url: str, port: int, database: str, user: str, password: str) -> None:
+        self.url: str = url
+        self.port: int = port
+        self.database: str = database
+        self.user: str = user
+        self.password: str = password
+        self.connection: Optional[connection] = None
+
+    def execute(self, query: str, data: Dict[str, object], column_collapse: bool = False, row_collapse: bool = False) -> Union[Dict[str, object], List[Dict[str, object]], List[object], object, None]:
+
+        with self.connection.cursor() as cursor:
+            cursor.execute(query, data)
+            rows = cursor.fetchall()
+
+            if len(rows) == 0:
+                return None
+
+            elif len(rows) == 1 and row_collapse:
+                if len(cursor.description) == 1 and column_collapse:
+                    return rows[0][0]
+                else:
+                    return {item.name: rows[0][index] for index, item in enumerate(cursor.description)}
+            else:
+                if len(cursor.description) == 1 and column_collapse:
+                    return [row[0] for row in rows]
+                else:
+                    return [{item.name: row[index] for index, item in enumerate(cursor.description)} for row in rows]
+
+    def start(self) -> None:
+        self.connection = connect(dbname=self.database, user=self.user, password=self.password, host=self.url, port=self.port)
+
+    def stop(self) -> None:
+        self.connection.close()
