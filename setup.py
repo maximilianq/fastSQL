bce178b2fe017f717f26af15a44e97cdddb0264f
Maximilian Quaeck <maximilian.quaeck@gmx.de>
2023-04-16 17:32:26 +0200

    added querry classes for better application structuring.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="fastsql",
-    version='0.0.0.2',
+    version='0.0.0.3',
     description="Library for SQL integration using annotations.",
     author="Maximilian Quaeck",
     author_email="maximilian.quaeck@gmx.de",
--- src/fastsql.py
+++ src/fastsql.py
@@ -1,57 +1,110 @@
-from psycopg import connect
-from psycopg.connection import Connection
-
-from typing import List, Dict, Optional, Union
-
+from psycopg2 import connect
+from psycopg2.extensions import connection as Connection
+from uuid import UUID as uuid, uuid4
+from re import compile
+from typing import Pattern, List, Tuple, Type, Dict
 
 class FastSQL:
 
-    def __init__(self, url: str, port: int, database: str, user: str, password: str) -> None:
-        self.url: str = url
+    def __init__(self, host: str, port: int, databse: str, username: str, password: str) -> None:
+        self.host: str = host
         self.port: int = port
-        self.database: str = database
-        self.user: str = user
+        self.database: str = databse
+        self.username: str = username
         self.password: str = password
-        self.connection: Optional[Connection] = None
-
-    def execute(self, query: str, data: Dict[str, object], column_collapse: bool = False, row_collapse: bool = False) -> Union[Dict[str, object], List[Dict[str, object]], List[object], object, None]:
+        self.connection: Connection = None
+        self.queries: List[Query] = []
+    
+    def execute(self, query: str, *args):
 
         with self.connection.cursor() as cursor:
-            cursor.execute(query, data)
-            rows = cursor.fetchall()
+            
+            if not args:
+                cursor.execute(query)
+            else:
+                cursor.execute(query, *args)
 
-            if len(rows) == 0:
+            if cursor.description:
+                return cursor.fetchall()
+            else:
                 return None
 
-            elif len(rows) == 1 and row_collapse:
-                if len(cursor.description) == 1 and column_collapse:
-                    return rows[0][0]
-                else:
-                    return {item.name: rows[0][index] for index, item in enumerate(cursor.description)}
-            else:
-                if len(cursor.description) == 1 and column_collapse:
-                    return [row[0] for row in rows]
-                else:
-                    return [{item.name: row[index] for index, item in enumerate(cursor.description)} for row in rows]
+    def include_query(self, query: 'Query'):
+        query.include(self)
+        self.queries.append(query)
 
-    def start(self) -> None:
-        self.connection = connect(dbname=self.database, user=self.user, password=self.password, host=self.url, port=self.port)
-
-    def stop(self) -> None:
-        self.connection.close()
-        self.connection.close()
-
-    def include_router(self, router: 'SQLRouter') -> None:
+    def include_router(self, router: 'SQLRouter'):
         router.include(self)
+        for query in router.queries:
+            self.queries.append(query)
 
+    def start(self):
+        self.connection = connect(dbname=self.database, user=self.username, password=self.password, host=self.host, port=self.port)
+        self.connection.autocommit = True
+
+        for query in self.queries:
+            query.prepare()
+    
+    def stop(self):
+        self.connection.close()
 
 class SQLRouter:
 
     def __init__(self) -> None:
         self.client: FastSQL = None
+        self.queries: List[Query] = []
+
+    def include(self, client: FastSQL):
+        self.client = client
     
-    def include(self, client: FastSQL) -> None:
+    def include_query(self, query: 'Query'):
+        query.include(self)
+        self.queries.append(query)
+
+    def execute(self, query: str, *args):
+        return self.client.execute(query, args)
+
+class Query:
+
+    pattern: Pattern = compile(r'\%\(([a-z]+)\)([a-z])')
+    types: Dict[str, Type] = {'s': str}
+
+    def __init__(self, query: str) -> None:
+
+        self.client: FastSQL | SQLRouter = None
+
+        self.uuid: uuid = uuid4()
+        
+        self.prepare_query: str = f'PREPARE "{str(self.uuid)}" AS {query};'
+        
+        self.parameters: List[Tuple[str, Type]] = [(name, self.types.get(type, None)) for name, type in set(self.pattern.findall(query))]
+        for index, (name, type) in enumerate(self.parameters):
+            self.prepare_query =  self.prepare_query.replace(f"%({name})s", f'${index + 1}')
+
+        self.execute_query: str = f'EXECUTE "{str(self.uuid)}"' + (f'({", ".join(["%s"] * len(self.parameters))})' if len(self.parameters) > 0 else '') + ';'
+
+    def include(self, client: FastSQL | SQLRouter):
         self.client = client
 
-    def execute(self, query: str, data: Dict[str, object], column_collapse: bool = False, row_collapse: bool = False) -> Union[Dict[str, object], List[Dict[str, object]], List[object], object, None]:
-        self.client.execute(query, data, column_collapse, row_collapse)
+    def prepare(self):
+        print(self.prepare_query)
+        self.client.execute(self.prepare_query)
+
+    def execute(self, **kwargs):
+        args: List[object] = [kwargs.get(name, None) for (name, type) in self.parameters]
+        return self.client.execute(self.execute_query, *args)
+    
+class SimpleQuery(Query):
+
+    def __init__(self, query: str) -> None:
+        super().__init__(query.rstrip(';'))
+
+class FileQuery(Query):
+
+    def __init__(self, path: str) -> None:
+
+        query: str = None
+        with open(path, 'r') as file:
+            query = file.read()
+
+        super().__init__(query.rstrip(';'))
